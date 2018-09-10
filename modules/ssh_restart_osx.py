4b1453c2077bb958c92d2e89bf2da694b3d76a3e
Akhliskun <labicidanut@gmail.com>
2018-09-10 13:29:21 +0300

    Initial SSH restart

---
+++ modules/ssh_restart_osx.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+import subprocess
+import sys
+
+OSX = "hosts.txt"
+# Ports are handled in ~/.ssh/config since we use OpenSSH
+COMMAND = "uname -a"
+
+def restart_osx():
+    with open(OSX, 'r', encoding='utf-8') as machines:
+        for HOST in machines:
+            ssh = subprocess.Popen(["ssh", "%s" % HOST, COMMAND],
+                                   shell=False,
+                                   stdout=subprocess.PIPE,
+                                   stderr=subprocess.PIPE)
+            result = ssh.stdout.readlines()
+
+            if not result:
+                error = ssh.stderr.readlines()
+                print(sys.stderr, "ERROR: %s" % error)
+            else:
+                print(result)
